e51969487cec0beac485303a5484010741681911
xlmnope <lmgiroud@gmail.com>
2019-03-16 09:44:15 -0700

    jaws connection

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,18 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Password123",
-  database: "burgers_db"
-});
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password123',
+    database: 'burgers_db'
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
